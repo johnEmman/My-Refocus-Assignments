ea0cd9a7414ce40af2b56b4d8ae5e9ffba0662af
johnEmman <ty.johnemmanuel.com>
2022-12-03 09:57:32 +0800

    Manual Testing for BMI Calculator

--- bmi_calculator.js
+++ bmi_calculator.js
@@ -1,13 +1,30 @@
+const Underweight = "Underweight";
+const Normal = "Normal";
+const Overweight = "Overweight";
+const Obese = "Obese";
+
+
 function bmi(weight, height) {
     let bmi = weight / (height**2);
     
+
+
     if(bmi < 18.5){
-      return "Underweight";
+      return Underweight;
     }else if (bmi < 25){
-      return "Normal";
+      return Normal;
     }else if (bmi < 30){
-      return "Overweight";
+      return Overweight;
     }else{
-      return "Obese";
+      return Obese;
     }
   }
+
+  function testBMI(){
+    const result = bmi(55, 5)
+    let expected =  Obese;
+
+    console.assert(result == expected, "Failed")
+  }
+
+  testBMI()
